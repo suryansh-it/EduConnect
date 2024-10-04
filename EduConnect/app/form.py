824119de1104b36ba2fb180581c8d76470516045
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-04 21:18:45 +0530

    vuew for signup doe

--- EduConnect/app/form.py
+++ EduConnect/app/form.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:                             #The Meta class provides metadata about the form.
+class CustomUserCreationForm(UserCreationForm): #A built-in Django form for user creation. 
+                                                #It includes built-in validation for password fields (password1,password2).
+    class Meta:                                 #The Meta class provides metadata about the form.
         model = User
         fields = ["username" , "email" , "password1" , "password2"]
--- EduConnect/app/views.py
+++ EduConnect/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .form import CustomUserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST) #Initialize the form with POST data.
+        if form.is_valid(): #validte the form
+            form.save()  # Save the new user to the database.
+            return redirect('login')   # Redirect to the login page.
+        
+    else :
+        form = CustomUserCreationForm() #Show the blank form for GET requests.
+
+    return render(request ,  "registration/signup.html" , {"form": form}) #render the form
